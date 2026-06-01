eaf2e4c2f0764fe51f294172830010b3af6ddcb3
ali1x3 <avilaalex371@gmail.com>
2026-06-01 14:50:29 +0800

    in the middle of authentication and permissions

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,122 @@
 from django.db import models
+from django.urls import reverse
+
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+import uuid
 
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text="Enter a book genre (e.g. Sience Fiction, French Poetry etc.)",
+    )
+
+    def __str__(self):
+        """String for refresenting the Model Object"""
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('genre-detail', args=[str(self.id)])
+    
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='genre_name_case_insensitive_unique',
+                violation_error_message = "Genre already exists (case insensitive match)"
+            ),
+        ]
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.RESTRICT, null=True)
+
+    summary = models.TextField(
+        max_length=1000, help_text="Enter a brief description of the book")
+
+    isbn = models.CharField('ISBN', max_length=13, unique=True,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                            '">ISBN number </a>')
+
+    genre = models.ManyToManyField(
+        Genre, help_text="Select a genre for this book"
+    )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='language_name_case_insensitive_unique',
+                violation_error_code="Language already exists (case insensitive match)"
+            )
+        ]
+
+def get_default_language():
+    language, create = Language.objects.get_or_create(
+        name="English"
+    )
+    return language.id
+
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unque ID for this particular book across whle libraray")
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    language = models.ForeignKey('Language', on_delete=models.RESTRICT, null=False, default=get_default_language)
+
+    LOAD_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAD_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    
